287462963c16ce0b4bad39b6b5e11b4d486a7712
agovi <anbugovi@gmail.com>
2020-06-03 16:59:28 -0700

    Update main.tf

--- storage/main.tf
+++ storage/main.tf
@@ -97,10 +97,10 @@ resource "null_resource" "post-config" {
 
   provisioner "local-exec" {
     command = <<EOT
-echo 'storage_account_name = "${azurerm_storage_account.sa.name}"' 
-echo 'container_name = "terraform-state"' 
-echo 'key = "terraform.tfstate"' 
-echo 'sas_token = "${data.azurerm_storage_account_sas.state.sas}"' 
+echo 'storage_account_name = "${azurerm_storage_account.sa.name}"' >> backend-config.txt
+echo 'container_name = "terraform-state"' >> backend-config.txt
+echo 'key = "terraform.tfstate"' >> backend-config.txt
+echo 'sas_token = "${data.azurerm_storage_account_sas.state.sas}"' >> backend-config.txt
 EOT
   }
 }
