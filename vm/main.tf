5bbb72894bfaac64708f494dc85cae1091ccd50d
agovi <anbugovi@gmail.com>
2020-06-03 16:48:54 -0700

    Add files via upload

--- vm/main.tf
+++ vm/main.tf
@@ -1,38 +1,36 @@
-##############
-# PROVIDERS
-##############
-
 # Configure the Microsoft Azure Provider 
 provider "azurerm" {
     version         = ">2.0.0"
     features    {}
 }
 
-# ##############
-# # Network Provisioning 
-# ##############
+# Create a resource group if it doesn’t exist
+resource "azurerm_resource_group" "rg" {
+    name                = var.az_resource_group
+    location            = var.az_region
+}
 
-# resource "azurerm_virtual_network" "vnet" {
-#   name                = "${var.sap_sid}-vnet"
-#   location            =  azurerm_resource_group.hana-resource-group.location
-#   resource_group_name =  azurerm_resource_group.hana-resource-group.name
-#   address_space       = ["10.0.0.0/21"]
-# }
+# Create virtual network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet-ado4sap"
+  location            =  azurerm_resource_group.rg.location
+  resource_group_name =  azurerm_resource_group.rg.name
+  address_space       = ["10.0.0.0/21"]
+}
 
-# resource "azurerm_subnet" "subnet" {
-#   name                      = "hdb-subnet"
-#   resource_group_name       = azurerm_resource_group.hana-resource-group.name
-#   virtual_network_name      = azurerm_virtual_network.vnet.name
-#   address_prefix            = "10.0.0.0/24"
-#   network_security_group_id = azurerm_network_security_group.sap_nsg[0].id
-# }
+resource "azurerm_subnet" "subnet" {
+  name                      = "sap-subnet"
+  resource_group_name       = azurerm_resource_group.rg.name
+  virtual_network_name      = azurerm_virtual_network.vnet.name
+  address_prefix            = "10.0.0.0/24"
+}
 
 
 # # Generate random text for a unique storage account name.
 # resource "random_id" "randomId" {
 #   keepers = {
 #     # Generate a new id only when a new resource group is defined.
-#     resource_group = var.az_resource_group
+#     resource_group = azurerm_resource_group.rg.name
 #   }
 
 #   byte_length = 8
@@ -54,7 +52,7 @@ provider "azurerm" {
 # # All disks that are in the storage_disk_sizes_gb list will be created
 # resource "azurerm_managed_disk" "disk" {
 #   count                = length(var.storage_disk_sizes_gb)
-#   name                 = "${var.machine_name}-disk${count.index}"
+#   name                 = "${var.name}-disk${count.index}"
 #   location             = var.az_region
 #   storage_account_type = "Premium_LRS"
 #   resource_group_name  = var.az_resource_group
@@ -71,55 +69,62 @@ provider "azurerm" {
 #   caching            = "ReadWrite"
 # }
 
-# # Create virtual machine
-# resource "azurerm_virtual_machine" "vm" {
-#   name                          = var.machine_name
-#   location                      = var.az_region
-#   resource_group_name           = var.az_resource_group
-#   network_interface_ids         = [var.nic_id]
-#   availability_set_id           = var.availability_set_id
-#   vm_size                       = var.vm_size
-#   delete_os_disk_on_termination = "true"
+# # Create network interface
+# resource "azurerm_network_interface" "nic" {
+#     name                      = "${var.name}-nic"
+#     location                  = var.az_region
+#     resource_group_name       = azurerm_resource_group.rg.name
 
-#   storage_os_disk {
-#     name              = "${var.machine_name}-OsDisk"
-#     caching           = "ReadWrite"
-#     create_option     = "FromImage"
-#     managed_disk_type = "Premium_LRS"
-#   }
-
-#   storage_image_reference {
-#     publisher = "SUSE"
-#     offer     = "sles-sap-12-sp5"
-#     sku       = "gen1"
-#     version   = "latest"
-#   }
-
-#   os_profile {
-#     computer_name  = var.machine_name
-#     admin_username = var.vm_user
-#   }
-
-#   os_profile_linux_config {
-#     disable_password_authentication = true
-
-#     ssh_keys {
-#       path     = "/home/${var.vm_user}/.ssh/authorized_keys"
-#       key_data = file(var.sshkey_path_public)
+#     ip_configuration {
+#         name                          = "${var.name}-ip"
+#         subnet_id                     = azurerm_subnet.subnet.id
+#         private_ip_address_allocation = "Dynamic"
 #     }
-#   }
 
-#   boot_diagnostics {
-#     enabled = "true"
-
-#     storage_uri = azurerm_storage_account.bootdiagstorageaccount.primary_blob_endpoint
-#   }
-
-#   tags = merge(
-#     {
-#       "${var.machine_type}" = ""
-#     },
-#     var.tags,
-#   )
+#     tags = {
+#         environment = "Terraform Demo"
+#     }
 # }
 
+# # Create virtual machine
+# resource "azurerm_virtual_machine" "vm" {
+# #    availability_set_id   = azurerm_availability_set.avsets.id
+#     name                  = var.az_resource_group
+#     location              = var.az_region
+#     resource_group_name   = azurerm_resource_group.rg.name
+#     network_interface_ids = [azurerm_network_interface.nic.id]
+#     vm_size               = var.vmsize
+
+#     storage_os_disk {
+#         name              = "${var.name}-OsDisk"
+#         caching           = "ReadWrite"
+#         create_option     = "FromImage"
+#         managed_disk_type = "Premium_LRS"
+#     }
+
+#     storage_image_reference {
+#         publisher = "RedHat"
+#         offer     = "RHEL-SAP-HA"
+#         sku       = "7.6"
+#         version   = "latest"
+#     }
+
+#     os_profile {
+#         computer_name  = var.name
+#         admin_username = "sapadmin"
+#         admin_password = "M1crosoft2019"
+#     }
+
+#     os_profile_linux_config {
+#         disable_password_authentication = false
+#     }
+
+#     boot_diagnostics {
+#         enabled = "true"
+#         storage_uri = azurerm_storage_account.bootdiagstorageaccount.primary_blob_endpoint
+#     }
+
+#     tags = {
+#         environment = "Terraform Demo"
+#     }
+# }
